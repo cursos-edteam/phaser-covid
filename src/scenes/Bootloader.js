a8b03a1639480ec320ad4e75bdc562374f09de63
Adrian <dbarra@agetic.gob.bo>
2021-01-29 13:36:34 -0400

    Eventos de teclado

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -14,6 +14,11 @@ class Bootloader extends Phaser.Scene {
   }
   create() {
     this.virus = this.add.image(100, 100, 'virus');
+    const { KeyCodes } = Phaser.Input.Keyboard;
+    this.teclaUP = this.input.keyboard.addKey(KeyCodes.UP);
+    this.teclaUP.on('down', () => {
+      console.log('Presionaste');
+    });
   }
   update(time, delta) {}
 }
