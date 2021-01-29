666bedd4da325617c174fe923d0041ed168dd02e
Adrian <dbarra@agetic.gob.bo>
2021-01-29 12:12:43 -0400

    cambios

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -1,3 +1,21 @@
-class Bootloader extends Phaser.Scene {}
+import Virus from '../assets/virus.png';
+
+class Bootloader extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'Bootloader'
+    });
+  }
+  init() {
+    console.log('Soy init');
+  }
+  preload() {
+    this.load.image('virus', Virus);
+  }
+  create() {
+    this.virus = this.add.image(100, 100, 'virus');
+  }
+  update(time, delta) {}
+}
 
 export default Bootloader;
